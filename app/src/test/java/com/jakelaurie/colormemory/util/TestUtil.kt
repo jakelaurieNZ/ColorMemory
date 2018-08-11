f3dca790d8f2416fe7f94f9357a8a363909fce8c
Jake Laurie <jake.gordon.laurie@gmail.com>
2018-08-12 07:22:32 +1200

    remove redundant visibility modifier

--- app/src/test/java/com/jakelaurie/colormemory/util/TestUtil.kt
+++ app/src/test/java/com/jakelaurie/colormemory/util/TestUtil.kt
@@ -4,7 +4,7 @@ import com.jakelaurie.colormemory.model.Score
 
 class ScoresTestUtil {
     companion object {
-        public fun createScores(count: Int, value: Int): List<Score> {
+        fun createScores(count: Int, value: Int): List<Score> {
             val list = mutableListOf<Score>()
             for (i in 0 until count) {
                 val score = Score(i.toString())
