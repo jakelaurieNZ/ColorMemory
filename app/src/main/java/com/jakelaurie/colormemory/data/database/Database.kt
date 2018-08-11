f65263657a1dbfdeaf3515b295711d516c4140ec
Jake Laurie <jake.gordon.laurie@gmail.com>
2018-08-12 06:41:36 +1200

    No more annoying exportschema error

--- app/src/main/java/com/jakelaurie/colormemory/data/database/Database.kt
+++ app/src/main/java/com/jakelaurie/colormemory/data/database/Database.kt
@@ -4,7 +4,7 @@ import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import com.jakelaurie.colormemory.model.Score
 
-@Database(entities = [Score::class], version = 1)
+@Database(entities = [Score::class], version = 1, exportSchema = false)
 abstract class Database : RoomDatabase() {
     abstract fun scoreDao(): ScoreDAO
 }
